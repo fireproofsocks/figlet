394169c5fff88c428ddec7f8e7435e5d0d3a8f0b
Everett Griffiths <everett@fireproofsocks.com>
2024-12-31 14:21:43 -0500

    Bump version; bump deps

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Figlet.MixProject do
   use Mix.Project
 
-  @version "0.3.1"
+  @version "0.3.2"
   @source_url "https://github.com/fireproofsocks/figlet"
 
   def project do
@@ -46,7 +46,7 @@ defmodule Figlet.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.29.0", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.36.1", only: :dev, runtime: false}
     ]
   end
 
